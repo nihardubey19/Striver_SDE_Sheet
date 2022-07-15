78069f30f1b40991812fad2d182313e55c7a6442
Nihar Dubey <nihardubey19@gmail.com>
2022-07-15 23:20:18 +0530

    day6

--- Day6/Linked_List_CycleII.java
+++ Day6/Linked_List_CycleII.java
@@ -12,3 +12,27 @@
         }
         return null;
     }
+
+// ----------------------------------------------------------------
+    
+    // using Time Complexity : O(n) and Space Complexity : O(1)
+
+    public ListNode detectCycle(ListNode head) {
+        if(head==null)
+            return head;
+        
+        ListNode slow = head, fast = head;
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(fast == slow){
+                ListNode slow2 = head;
+                while(slow2!=slow){
+                    slow2 = slow2.next;
+                    slow = slow.next;
+                }
+                return slow;
+            }
+        }
+        return null;
+    }
