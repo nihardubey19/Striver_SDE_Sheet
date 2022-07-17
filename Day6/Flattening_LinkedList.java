91b550a78ccb45b0e044cf721f6e8890ad3b88b5
Nihar Dubey <nihardubey19@gmail.com>
2022-07-18 00:26:18 +0530

    day6

--- Day6/Flattening_LinkedList.java
+++ Day6/Flattening_LinkedList.java
@@ -27,3 +27,39 @@
         }
         return dummyHead.bottom;
     }
+
+// More Optimized Solution
+// using Time : O(summation of all nodes) and Space : O(1)
+
+    Node flatten(Node root)
+    {
+	// Your code here
+	    if(root==null || root.next==null)
+	        return root;
+	    root.next  = flatten(root.next);
+	    
+	    root = mergeTwoLists(root, root.next);
+	    return root;
+    }
+    
+    Node mergeTwoLists(Node a, Node b){
+        Node temp = new Node(0);
+        Node res = temp;
+        
+        while(a!=null && b!=null){
+            if(a.data < b.data){
+                temp.bottom = a;
+                a = a.bottom;
+            }else{
+                temp.bottom = b;
+                b = b.bottom;
+            }
+            temp = temp.bottom;
+        }
+        
+        if(a!=null)
+            temp.bottom = a;
+        else
+            temp.bottom = b;
+        return res.bottom;
+    }
