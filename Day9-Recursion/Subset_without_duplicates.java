736a6cb28d18d295a40f866eded7e19f5ea6892a
Nihar Dubey <nihardubey19@gmail.com>
2022-09-11 16:02:49 +0530

    day9

---
+++ Day9-Recursion/Subset_without_duplicates.java
@@ -0,0 +1,26 @@
+   // Time Complexity : O(2^n)
+   // Space Complexity : O(2^n), Auxilary Space O(n) for recursion
+
+    private List<List<Integer>> ans;
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        ans = new ArrayList<>();
+        Arrays.sort(nums);
+        helper(0, nums, new LinkedList<>());
+        return ans;
+    }
+    
+    private void helper(int idx, int[] nums, LinkedList<Integer> temp){
+        if(idx==nums.length){
+            ans.add(new ArrayList<>(temp));
+            return;
+        }
+        
+        temp.add(nums[idx]);
+        helper(idx+1, nums, temp);
+        int prev = nums[idx];
+        while(idx<nums.length && prev==nums[idx]){
+            idx++;
+        }
+        temp.removeLast();
+        helper(idx, nums, temp);
+    }
