295664385d54eab1f766ed1951c0588917bb9c04
Nihar Dubey <nihardubey19@gmail.com>
2022-09-15 20:32:22 +0530

    day13

--- Day13-Stack and Queue/Implement_Stack_Using_2Queues.java
+++ Day13-Stack and Queue/Implement_Stack_Using_2Queues.java
@@ -1,5 +1,6 @@
     // Time Complexity : O(1) in push Ops, O(n) in pop Ops
     // Space Complexity : O(2n) for 2 Queue DS
+    // Using 2 queues 
 
     Queue<Integer> q1, q2;
     int top;
@@ -32,3 +33,33 @@
     public boolean empty() {
         return q1.isEmpty() ? true : false;
     }
+
+// -------------------------------------------------
+
+    // Time Complexity  : O(n)
+    // Space Complexity : O(n)
+   
+class MyStack {
+    Queue<Integer> q;
+    public MyStack() {
+        q = new LinkedList<>();
+    }
+    
+    public void push(int x) {
+        q.add(x);
+        for(int i=0; i<q.size()-1; i++)
+            q.add(q.poll());
+    }
+    
+    public int pop() {
+        return q.poll();
+    }
+    
+    public int top() {
+        return q.peek();
+    }
+    
+    public boolean empty() {
+        return q.isEmpty();
+    }
+}
