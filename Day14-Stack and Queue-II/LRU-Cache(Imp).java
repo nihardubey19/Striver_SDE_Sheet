844ebbfce3da5b6e6ca5715010a1808bed176b7a
Nihar Dubey <nihardubey19@gmail.com>
2022-09-18 20:12:00 +0530

    day14

---
+++ Day14-Stack and Queue-II/LRU-Cache(Imp).java
@@ -0,0 +1,58 @@
+    // Using hashMap and Doubly LinkedList
+    // with time Complexity of get and put in Amortized O(1)
+class LRUCache {
+    Node head = new Node(0,0);
+    Node tail = new Node(0,0);
+    Map<Integer,Node> map = new HashMap<>();
+    int capacity;
+    
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        head.next = tail;
+        tail.prev = head;
+    }
+    
+    public int get(int key) {
+        if(!map.containsKey(key))
+            return -1;
+        Node curr = map.get(key);
+        remove(curr);
+        insert(curr);
+        return curr.value;
+    }
+    
+    public void put(int key, int value) {
+        if(map.containsKey(key)){
+            Node node = map.get(key);
+            remove(node);
+        }
+        if(map.size()==capacity){
+            remove(tail.prev);
+        }
+        insert(new Node(key, value));
+    }
+    
+    private void remove(Node node){
+        map.remove(node.key);
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+    
+    private void insert(Node node){
+        map.put(node.key, node);
+        Node temp = head.next;
+        head.next = node;
+        node.prev = head;
+        node.next = temp;
+        temp.prev = node;
+    }
+    
+    static class Node{
+        Node prev, next;
+        int key, value;
+        public Node(int _key, int _value){
+            key = _key;
+            value = _value;
+        }
+    }
+}
