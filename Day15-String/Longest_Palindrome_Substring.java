394f24e194be3b2b0bd3e61d0d7f21be15d1da17
Nihar Dubey <nihardubey19@gmail.com>
2022-09-24 23:26:22 +0530

    day15

---
+++ Day15-String/Longest_Palindrome_Substring.java
@@ -0,0 +1,32 @@
+    // time Complexity : O(n^2)
+    // Space Complexity : O(1)
+
+    public String longestPalindrome(String s) {
+        if(s==null || s.length()<1)
+            return null;
+        
+        int start=0, end=0;
+        //aabbaa
+        //racecar
+        for(int i=0; i<s.length(); i++){
+            int len1 = expandFromMiddle(s,i,i);
+            int len2 = expandFromMiddle(s,i,i+1);
+            
+            int len = Math.max(len1, len2);
+            
+            if(len>end-start){
+                start = i-(len-1)/2;
+                end = i+(len)/2;
+            }
+        }
+        
+        return s.substring(start,end+1);        
+    }
+    
+    private int expandFromMiddle(String s, int left, int right){
+        while(left>=0 && right<s.length() && s.charAt(left)==s.charAt(right)){
+            left--;
+            right++;
+        }
+        return right-left-1;
+    }
