5e0d4947c1286326d86019c8a5ec2c499f58c139
Nihar Dubey <nihardubey19@gmail.com>
2022-11-11 16:44:31 +0530

    day 20

---
+++ Day20-BST/sortedArrayToBST.java
@@ -0,0 +1,16 @@
+    // Time Complexity : O(n)
+    // Space Complexity : O(n)
+
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return helper(0, nums.length-1, nums);
+    }
+
+    private TreeNode helper(int low, int high, int[] nums){
+        if(low>high) return null;
+        if(low==high) return new TreeNode(nums[low]);
+        int mid = low + (high - low)/2;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = helper(low, mid-1, nums);
+        root.right = helper(mid+1, high, nums);
+        return root;
+    }
